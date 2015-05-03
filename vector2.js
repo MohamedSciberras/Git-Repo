35924e0283fef65cd2116a5dbae14b5c3435f52f
MohamedSciberras <mohamed.sciberras@hotmail.com>
2015-05-03 10:18:35 +1000

    added extras preparing for level
    Skipping vector2 and moving to JSON

---
+++ vector2.js
@@ -0,0 +1,30 @@
+var Vector2 = function()
+{
+	this.x = 0;
+	this.y = 0;
+};
+
+function set(x, y)
+{
+
+}
+
+function normalize()
+{
+
+}
+
+function add(v2)
+{
+
+}
+
+function subtract(v2)
+{
+
+}
+
+function multiplyScalar(num)
+{
+
+}
